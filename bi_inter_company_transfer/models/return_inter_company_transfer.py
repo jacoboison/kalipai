4825c94bb46e6ccdc8b818ca1b2dd762fcee8d1f
LuisMendezG <luis.mendez@integritas.mx>
2026-03-25 17:25:00 +0100

    Update 19

--- bi_inter_company_transfer/models/return_inter_company_transfer.py
+++ bi_inter_company_transfer/models/return_inter_company_transfer.py
@@ -9,7 +9,7 @@ from odoo.exceptions import UserError, ValidationError , Warning
 from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.tools.misc import formatLang
 from odoo.tools import html2plaintext
-import odoo.addons.decimal_precision as dp
+#import odoo.addons.decimal_precision as dp
 
 
 class ReturnPickingLine(models.Model):
